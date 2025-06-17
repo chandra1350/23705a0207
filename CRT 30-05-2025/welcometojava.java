2ed17679ef8d3aca089d93724ae10689eefb22f5
chandra1350 <chandrasekharsura2424@gmail.com>
2025-06-17 11:45:01 +0530

    TODAY FOLDER

--- CRT 30-05-2025/welcometojava.java
+++ CRT 30-05-2025/welcometojava.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args){
-        System.out.println("Welcome to Java Program");
+        System.out.println("JAVA");
     }
 }
